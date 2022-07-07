96e8cc76af73043d33b3e9e11029d3905135936b
Parijat Niyogy <tito@Parijats-MacBook-Air.local>
2022-07-07 22:34:04 +0530

    Pre test routes

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,7 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
-app.get('/api/goals', (req,res) => {
-    res.json({"message" : "Get goals"})
-})
+app.use('/api/goals', require('./routes/goalRoutes'))
+// if we hit api/goals then it is going to look into the following folder for the file
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
