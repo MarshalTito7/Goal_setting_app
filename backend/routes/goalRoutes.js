6c68c54341e599ebabac29b37ca4fa4bbf5f8c06
Parijat Niyogy <tito@Parijats-MacBook-Air.local>
2022-07-08 00:21:42 +0530

    Optimised

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal} = require('../controller/goalController')
 
-router.get('/', (req,res) => {
-    res.status(200).json({"message" : "Get goals"})
-})
+// router.get('/', getGoals)
+// router.post('/', setGoal)
+// These two can be replace as
+router.route('/').get(getGoals).post(setGoal)
 
-router.post('/', (req,res) => {
-    res.status(200).json({"message" : "Set goal"})
-})
-
-router.put('/:id', (req,res) => {
-    res.status(200).json({"message" : `Update goal ${req.params.id}`})
-})
-
-router.delete('/:id', (req,res) => {
-    res.status(200).json({"message" : `Delete goal ${req.params.id}`})
-})
+// router.put('/:id', updateGoal)
+// router.delete('/:id', deleteGoal)
+// These two can be replace as
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
