cb0d6c145ae5ab96e73fd7ce722b771c73aafc90
MarshalTito7 <parijat.niyogy7@gmail.com>
2022-07-27 23:57:57 +0530

    few changes made

--- backend/model/goalModel.js
+++ backend/model/goalModel.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose')
 
 const goalSchema = mongoose.Schema({
     text: {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            // We are saying that we want the user to be an objectID
+            required: true,
+            ref: 'User'
+            // We need to mention which mo
+        },
         type: String,
         required: [true, 'Please add a text value']
     },
